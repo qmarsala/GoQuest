eff897872d09aad7cd86949cddc7a97ab16098fb
Quinten Marsala <quinten.marsala@gmail.com>
2024-04-15 10:37:02 -0700

    declare a few types we will need

--- main.go
+++ main.go
@@ -3,5 +3,34 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("hello world")
+}
+
+type Ability struct {
+	Name        string
+	Description string
+	Score       int
+	Modifier    int
+}
+
+type Skill struct {
+	Name        string
+	Description string
+	Score       int
+	Modifier    int
+}
+
+type Background struct {
+	Name        string
+	Description string
+	SkillBoosts map[string]int
+}
+
+type Player struct {
+	Name        string
+	Description string
+	Motivation  string
+	Catalyst    string
+	Skills      [28]Skill
+	Abilities   [3]Ability
 }
