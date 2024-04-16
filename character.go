87d30c518379274eb950c5411b83bcc72f5ee4b9
Quinten Marsala <quinten.marsala@gmail.com>
2024-04-15 21:46:12 -0700

    add default skill and ability values

--- character.go
+++ character.go
@@ -18,6 +18,7 @@ type Skill struct {
 	Description string
 	Score       int
 	Modifier    int
+	Used        bool
 }
 
 type Background struct {
@@ -35,6 +36,177 @@ type Character struct {
 	Abilities   map[string]Ability
 }
 
+var defaultAbilities = map[string]Ability{
+	"Agility": {
+		Name:        "Agility",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+	},
+	"Strength": {
+		Name:        "Strength",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+	},
+	"Intellect": {
+		Name:        "Intellect",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+	},
+}
+
+var defaultSkills = map[string]Skill{
+	"Archaeology": {
+		Name:        "Archaeology",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Attack": {
+		Name:        "Attack",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Cooking": {
+		Name:        "Cooking",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Crafting": {
+		Name:        "Crafting",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Defense": {
+		Name:        "Defense",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Dungeoneering": {
+		Name:        "Dungeoneering",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Farming": {
+		Name:        "Farming",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Fishing": {
+		Name:        "Fishing",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Fletching": {
+		Name:        "Fletching",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Herblore": {
+		Name:        "Herblore",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Hunting": {
+		Name:        "Hunting",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Magic": {
+		Name:        "Magic",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Mining": {
+		Name:        "Mining",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Prayer": {
+		Name:        "Prayer",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Ranged": {
+		Name:        "Ranged",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Runecrafting": {
+		Name:        "Runecrafting",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Slayer": {
+		Name:        "Slayer",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Smithing": {
+		Name:        "Smithing",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Summoning": {
+		Name:        "Summoning",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Thieving": {
+		Name:        "Thieving",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+	"Woodcutting": {
+		Name:        "Woodcutting",
+		Description: "",
+		Score:       1,
+		Modifier:    0,
+		Used:        false,
+	},
+}
+
 const characterSaveFilePath string = "./character.json"
 
 func LoadCharacter() {
@@ -47,4 +219,16 @@ func LoadCharacter() {
 func SaveCharacter(character Character) {
 	jsonDat, _ := json.Marshal(character)
 	os.WriteFile(characterSaveFilePath, jsonDat, 0644)
+}
+
+func CreateNewCharacter(name string, description string, motivation string, catalyst string) Character {
+	newCharacter := Character{
+		Name:        name,
+		Description: description,
+		Motivation:  motivation,
+		Catalyst:    catalyst,
+		Skills:      defaultSkills,
+		Abilities:   defaultAbilities,
+	}
+	return newCharacter
 }
--- main.go
+++ main.go
@@ -8,25 +8,12 @@ import (
 )
 
 func main() {
-	c := createNewCharacter()
-	SaveCharacter(c)
-}
-
-func createNewCharacter() Character {
 	name := readString("Name:")
 	description := readString("Description:")
 	motivation := readString("Motivation:")
 	catalyst := readString("Catalyst:")
-
-	p := Character{
-		Name:        name,
-		Description: description,
-		Motivation:  motivation,
-		Catalyst:    catalyst,
-		Skills:      map[string]Skill{},
-		Abilities:   map[string]Ability{},
-	}
-	return p
+	c := CreateNewCharacter(name, description, motivation, catalyst)
+	SaveCharacter(c)
 }
 
 func readString(prompt string) string {
